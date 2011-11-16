9ee77ac3a86fd56a224350e886aabf6f58abe2d4
Guillermo Alvarado <stallion.ak47@gmail.com>
2011-11-16 16:15:05 -0600

    S #5 - Fixes

--- jobsite/main/forms.py
+++ jobsite/main/forms.py
@@ -1,12 +1,5 @@
 from django import forms
 
-ACCOUNT_CHOICES = (
-    (1, 'Player'),
-    (2, 'Clinic'),
-    (3, 'Coach'),
-    (4, 'Court owner'),
-)
-
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
     sender = forms.EmailField()
